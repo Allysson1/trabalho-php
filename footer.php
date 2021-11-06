9d4b85baf2572c453d8004918174d6107005a5cd
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2021-11-06 18:12:13 -0300

    iniciando-o-projeto

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../trabalho-php/css/estilo.css">
+    </head>
+    <body>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Conhecimento Básico PHP</title>
+        <link rel="stylesheet" href="..trabalho-php/css/estilo.css">
+    </head>
+    <body>
+
+    <header>
+<h1>
+PHP
+</h1>
+    </header>
+
+<article>
+<h1>Conceito de Estrutura de dados</h1>
+
+<h1>Conceito de lista</h1>
+<p>conceito</p>
+
+<p>Como usar lista no php 
+    exemplo em codigo
+</p>
+
+<h1>conceito de fila</h1>
+
+<p>como usar fila no php
+exemplo em codigo
+</p>
+
+<h1>Conceito de pilha</h1>
+
+<p>como usar pilha no php
+exemplo em codigo
+</p>
+
+<h1>Conceito de árvore</h1>
+
+<p>como usar árvore no php
+exemplo em codigo
+</p>
+
+</article>   
+
+
+<section>
+<ul>
+<li class="principal"><a>Estrutura de dados</a></li>
+<li class="sub">conceito<li>
+
+<li class="principal">Conceito de lista</li>
+<li class="sub">como usar lista no php</li>
+<li class="sub">exemplo em codigo</li>
+
+<li class="principal">conceito de fila</li>
+<li class="sub">como usar fila no php</li>
+<li class="sub">exemplo em codigo</li>
+
+<li class="principal">conceito de Pilha</li>
+<li class="sub">como usar Pilha no php</li>
+<li class="sub">exemplo em codigo</li>
+
+<li class="principal">conceito de Árvore</li>
+<li class="sub">como usar árvore no php</li>
+<li class="sub">exemplo em codigo</li>
+
+</ul>
+</section>
+
+
+    </body>
+</html>
