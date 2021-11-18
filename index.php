86692a59b2d273797fc34a45a2d9002af510b091
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2021-11-18 19:03:59 -0300

    adicionando-códigos-de-fila-e-pilha

--- index.php
+++ index.php
@@ -37,9 +37,9 @@
         <!-- lista -->
         <h1 id="lista">Conceito de lista</h1>
         <p id="conceito-lista">A lista em php pode ser interpretada como uma
-             lista de compras que você irá fazer no mercado. Com a lista em 
-             php é possível adicionar limites aos itens que nela serão colocados.
-            </p>
+            lista de compras que você irá fazer no mercado. Com a lista em
+            php é possível adicionar limites aos itens que nela serão colocados.
+        </p>
         <p id="lista-code">exemplo em codigo</p>
 
 
@@ -52,9 +52,10 @@
         <p>Exemplo:</p>
 
 
-        <img src="" id="fila-code" alt="Imagem mostrando um exemplo de fila em php">
+        <img src="../trabalho-php/images/filacode.png" id="fila-code" alt="Imagem mostrando um exemplo de fila em php">
 
-
+        <p>Obs: A tag "print_r" exibe a ordem de cada item alocado além do conteúdo da lista. É possivel que o resultado apareça como o exemplo a seguir:</p>
+        <p> "Array ( [0] => Allysson [1] => Ana [2] => Júlio [3] => Nayara ) )"</p>
 
 
 
@@ -66,20 +67,30 @@
             o primeiro que você coloca vira base para os outros, logo, o primeiro
             livro adicionado se torna o último da pilha, enquanto o último
             adicionado fica por cima, se tornando o primeiro da pilha.</p>
-        <p id="pilha-code">exemplo em codigo</p>
+        <p id="pilha-code">exemplo:</p>
 
+        <img src="../trabalho-php/images/pilhacode.png" id="pilha-code" alt="Imagem mostrando um exemplo de pilha em php">
 
+        <p>Seguindo a lógica de que o ultimo colocado é o que fica por
+            cima em uma pilha,o resultado no navegador para este código
+            é "o primeiro da pilha é o prato 4", lembrando que a posição "$pilha[0]" é a primeira. </p>
+
+        <p>Agora um exemplo inserindo mais itens na pilha:</p>
+
+        <img src="../trabalho-php/images/pilhacode2.png" id="pilha-code" alt="Imagem mostrando um exemplo de pilha em php">
+
+        <p>Agora o reslutado de $pilha[0] passa a ser "o primeiro da pilha é o prato 7".</p>
 
         <!-- arvore -->
         <h1 id="arvore">Conceito de árvore</h1>
-        <p id="conceito-arvore">Seu uso é mais voltado a dados não lineares que 
-            podem ter diversas variáveis. A arvore é composta por vários nós que 
+        <p id="conceito-arvore">Seu uso é mais voltado a dados não lineares que
+            podem ter diversas variáveis. A arvore é composta por vários nós que
             possuem ligação com o elemento raiz. Um exemplo de árvore muito
-             conhecida por todos é a árvore genealógica, onde a partir do termo 
-             Pai e Mãe, surgem vários galhos mostrando as gerações que vieram 
-             posteriormente. A árvore é um conceito muito mais fácil de lidar, 
-             uma vez que é usada em várias outros tipos de casos como Banco de 
-             dados e pasta de arquivos dentro de outras pastas.</p>
+            conhecida por todos é a árvore genealógica, onde a partir do termo
+            Pai e Mãe, surgem vários galhos mostrando as gerações que vieram
+            posteriormente. A árvore é um conceito muito mais fácil de lidar,
+            uma vez que é usada em várias outros tipos de casos como Banco de
+            dados e pasta de arquivos dentro de outras pastas.</p>
         <p id="arvore-code">exemplo em codigo</p>
 
     </article>
